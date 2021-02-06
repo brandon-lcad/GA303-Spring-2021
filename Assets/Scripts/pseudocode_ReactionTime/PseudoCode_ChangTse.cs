4fdf527f3c2fe222f2bf0c8d57cb7b210efe8d6f
Anton <Changtse@lcad.edu>
2021-02-05 20:03:42 -0800

    Posted my pseudocode into the pseudocode folder

---
+++ Assets/Scripts/pseudocode_ReactionTime/PseudoCode_ChangTse.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PseudoCode_ChangTse : MonoBehaviour
+{
+    //Introduce the game
+    //Explain the rules
+        //IF player clicks mouse (Start Countdown timer == TT)(change screen to red)
+            //i = generate random number
+            //IF (TT <= i and Player clicks mouse) 
+                //Post "clicked to soon"
+                //Start again
+        //IF TT = i (change screen to green)(Start reaction Timer == RT)
+        //IF player clicks mouse (Stop RT) (Post RT)
+
+
+}
