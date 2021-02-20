bb07a0c8a1df5d9ee077eefecff1a18e73aa7cf4
Abdalla Talaat <abdallahtalaat@lcad.edu>
2021-02-19 19:21:20 -0800

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/NumberWizard_Homework/NumberWizard_A.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard_A: MonoBehaviour
+
+{
+    int min = 1;
+    int max = 20;
+    int guess = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(" Welcome To number Wizard ");
+        Debug.Log(" Pick a number between" + min + "and" + max);
+        Debug.Log(" I bet I can guess your number ");
+        Debug.Log(" Pick Up or Down if your number is low or high");
+        Debug.Log(" Press Enter if your number is correct");
+        Debug.Log(" Is your number" + guess + "?");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess; //Now the minimum is equal to 10 (now minimum takes place of the guess, the new min is 10)
+            guess = (max + min) / 2;
+            Debug.Log("Is your number" + guess + "?");
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess; //Now the minimum is equal to 10 (now maximum takes place of the guess, the new max is 10)
+            guess = (max + min) / 2;
+            Debug.Log("Is your number" + guess + "?");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return)) 
+        { 
+         Debug.Log("I got it. Your number is " + guess);
+        }
+    }
+
+}
---
+++ Assets/Scripts/NumberWizard_Homework/NumberWizard_AbdallaTalaat.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard_AbdallaTalaat : MonoBehaviour
+{
+    int max = 30; 
+    int min = 1;
+    int guess = 15; 
+
+    void Start()
+    {
+        Debug.Log("Hello there travel!");
+
+        Debug.Log("Pick a number between" + min + " and " + max + " and I know I can tell you what it is.");
+
+        Debug.Log("Is your number higher than, lower than, or exactly " + guess + "?");
+
+        Debug.Log("If it's higher, press UP. If it's lower, press DOWN. If it's correct, press Enter.");
+
+    }
+
+   
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))  
+        {
+            min = guess;
+            guess = (max + min) / 2;
+            Debug.Log("Higher? Is your number " + guess + "?");
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            guess = (max + min) / 2;
+            Debug.Log("Lower? your number is " + guess + "?");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("YUS, I got your number " + guess + "!");
+        }
+    }
+}
