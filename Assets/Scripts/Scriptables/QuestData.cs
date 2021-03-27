6a423cd8340eca56252d59dfa540778024d64dae
Tay <tamarallen@lcad.edu>
2021-03-26 19:03:12 -0700

    Initial commit
    Sharing project

---
+++ Assets/Scripts/Scriptables/QuestData.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Scriptables
+{
+    [CreateAssetMenu(fileName = "new Quest", menuName = "Game Systems/Quests")]
+    public class QuestData : GameContent
+    {
+        public enum RewardType
+        {
+            Currency,
+            Reputation,
+            Reagent
+        }
+
+        public int questObjective; 
+        public int invQuantity;
+
+        public bool questTimed;
+        public int questTimer;
+        public bool questActive;
+        public bool questComplete;
+
+        public int rewardAmount;
+        public Sprite rewardImage;
+        public RewardType rewardType;
+    }
+}
+
+
+
---
+++ Assets/Scripts/Settings/CustomGameSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace Settings
+{
+    public static class CustomGameSettings
+    {
+        // public enum ContentType { Loot, Quest }
+        
+        public const string QUEST_ASSETS_PATH = "Assets/Resources";
+        public const string ITEM_ASSETS_PATH = "Assets/Resources";
+        public const string STATIC_ASSETS_PATH = "Assets/StaticAssets";
+        public const string TOOL_ICONS_PATH = STATIC_ASSETS_PATH + "/ToolIcons";
+        public const string ITEM_CREATOR_ICON_PATH = TOOL_ICONS_PATH + "/loot.png";
+        public const string QUEST_CREATOR_ICON_PATH = TOOL_ICONS_PATH + "/quest.png";
+    
+    }
+}
