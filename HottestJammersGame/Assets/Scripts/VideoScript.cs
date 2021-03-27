912b1fda0893cb5b26284fb731a303b72f3c2a74
Heather Wright <heatherwright@lcad.edu>
2021-03-26 18:38:04 -0700

    Added Video Tracking Script
    Progresses to next scene when video ends

---
+++ HottestJammersGame/Assets/Scripts/VideoScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class VideoScript : MonoBehaviour
+{
+    VideoPlayer video;
+    public string sceneName;
+
+    void Awake()
+    {
+        video = GetComponent<VideoPlayer>();
+        video.Play();
+        video.loopPointReached += CheckOver;
+    }
+
+
+    void CheckOver(UnityEngine.Video.VideoPlayer vp)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
