b13eed7a4ba79942f322ac2b7d9eb929c80f31d4
Hikari <oishi.hikari@gmail.com>
2021-04-23 15:15:29 -0700

    Created scripts that will reset the Convo at StartMenu

---
+++ HottestJammersGame/Assets/Scripts/Resetter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Resetter : MonoBehaviour
+{
+    public Conversation startingConversation;
+    public static Resetter ResetterInstance;
+
+    void Awake()
+    {
+        if (ResetterInstance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            ResetterInstance = this;
+        }
+        else if (ResetterInstance != this)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+      if (scene.name == "Introduction")
+      {
+        GameObject dm = GameObject.Find("DialogueManager");
+        dm.SendMessage("ChangeConversation", startingConversation);
+      }
+    }
+}
